161e96445bd0ca03e4b2d4fb0553013dda5367de
frgryr2001 <nhanle.lx@gmail.com>
2022-01-02 00:10:38 +0700

    database

--- database/esdc.sql
+++ database/esdc.sql
@@ -12,16 +12,16 @@ START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+-- /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+-- /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+-- /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+-- /*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `esdc`
 --
-CREATE DATABASE IF NOT EXISTS `esdc` DEFAULT CHARACTER SET utf8 COLLATE utf8_vietnamese_ci;
-USE `esdc`;
+CREATE DATABASE IF NOT EXISTS `cnpm` DEFAULT CHARACTER SET utf8 COLLATE utf8_vietnamese_ci;
+USE `cnpm`;
 
 -- --------------------------------------------------------
 
@@ -29,22 +29,25 @@ USE `esdc`;
 -- Table structure for table `admin`
 --
 
-DROP TABLE IF EXISTS `admin`;
-CREATE TABLE IF NOT EXISTS `admin` (
-  `username` varchar(32) COLLATE utf8_vietnamese_ci NOT NULL,
-  `password` varchar(32) COLLATE utf8_vietnamese_ci DEFAULT NULL,
+DROP TABLE IF EXISTS `account`;
+CREATE TABLE IF NOT EXISTS `account` (
+  `id_account` varchar(32) COLLATE utf8_vietnamese_ci NOT NULL,
+  `username` varchar(32) COLLATE utf8_vietnamese_ci DEFAULT NULL,
+  `email` varchar(32) COLLATE utf8_vietnamese_ci DEFAULT NULL,
+  `password` varchar(100) COLLATE utf8_vietnamese_ci DEFAULT NULL,
   `name` varchar(100) COLLATE utf8_vietnamese_ci DEFAULT NULL,
-  PRIMARY KEY (`username`)
+  `phone` int DEFAULT NULL,
+  `address` varchar(100) COLLATE utf8_vietnamese_ci DEFAULT NULL,
+  `role` int(11) DEFAULT NULL, -- role 0 la admin , role 1 la nhanvien kho, role 2 la nhanvien bán hàng
+  `salary` double DEFAULT NULL,
+  `token` varchar(100) COLLATE utf8_vietnamese_ci DEFAULT NULL,
+  PRIMARY KEY (`id_account`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_vietnamese_ci;
 
 --
 -- Dumping data for table `admin`
 --
 
-INSERT INTO `admin` (`username`, `password`, `name`) VALUES
-('admin', 'admin', 'Đậu Đăng Sơn'),
-('admin1', 'admin1', 'Lê Hoàng Nhân'),
-('admin2', 'admin2', 'Đại Hiệp');
 
 -- --------------------------------------------------------
 
@@ -52,22 +55,20 @@ INSERT INTO `admin` (`username`, `password`, `name`) VALUES
 -- Table structure for table `bill`
 --
 
-DROP TABLE IF EXISTS `bill`;
-CREATE TABLE IF NOT EXISTS `bill` (
-  `billId` int(11) NOT NULL AUTO_INCREMENT,
-  `userId` int(11) DEFAULT NULL,
+DROP TABLE IF EXISTS `order`;
+CREATE TABLE IF NOT EXISTS `order` (
+  `id_order` int(11) NOT NULL AUTO_INCREMENT,
+  `id_account` int(11) DEFAULT NULL,
+  `status` int(11) DEFAULT NULL,
   `time` date NOT NULL DEFAULT current_timestamp(),
-  PRIMARY KEY (`billId`),
-  KEY `userId` (`userId`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_vietnamese_ci;
+  `total` double DEFAULT NULL,
+  PRIMARY KEY (`id_order`)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8 COLLATE=utf8_vietnamese_ci;
 
 --
 -- Dumping data for table `bill`
 --
 
-INSERT INTO `bill` (`billId`, `userId`, `time`) VALUES
-(1, 1, '2021-11-16'),
-(2, 1, '2021-11-16');
 
 -- --------------------------------------------------------
 
@@ -75,21 +76,21 @@ INSERT INTO `bill` (`billId`, `userId`, `time`) VALUES
 -- Table structure for table `bill_detail`
 --
 
-DROP TABLE IF EXISTS `bill_detail`;
-CREATE TABLE IF NOT EXISTS `bill_detail` (
-  `billId` int(11) DEFAULT NULL,
+DROP TABLE IF EXISTS `order_detail`;
+CREATE TABLE IF NOT EXISTS `order_detail` (
+  `id_order_detail` int(11) DEFAULT NULL,
+  `id_product` int(11) DEFAULT NULL,
   `qty` int(11) DEFAULT NULL,
-  `productName` varchar(1000) COLLATE utf8_vietnamese_ci DEFAULT NULL,
-  `productPrice` int(11) DEFAULT NULL,
-  KEY `billId` (`billId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_vietnamese_ci;
+  `serial` varchar(1000) COLLATE utf8_vietnamese_ci DEFAULT NULL, -- mã bảo hành
+  `productPrice` double DEFAULT NULL,
+  PRIMARY KEY (`id_order_detail`)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8 COLLATE=utf8_vietnamese_ci;
 
 --
 -- Dumping data for table `bill_detail`
 --
 
-INSERT INTO `bill_detail` (`billId`, `qty`, `productName`, `productPrice`) VALUES
-(1, 2, 'Tai nghe True Wireless Chống ồn Soundpeats T2\r\n', 1200000);
+
 
 -- --------------------------------------------------------
 
@@ -97,33 +98,16 @@ INSERT INTO `bill_detail` (`billId`, `qty`, `productName`, `productPrice`) VALUE
 -- Table structure for table `brand`
 --
 
-DROP TABLE IF EXISTS `brand`;
-CREATE TABLE IF NOT EXISTS `brand` (
-  `brandId` int(11) NOT NULL AUTO_INCREMENT,
-  `brand` varchar(255) COLLATE utf8_vietnamese_ci DEFAULT NULL,
-  PRIMARY KEY (`brandId`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8 COLLATE=utf8_vietnamese_ci;
+CREATE TABLE `category` (
+  `id_category` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_vietnamese_ci DEFAULT NULL,
+  PRIMARY KEY (`id_category`)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8 COLLATE=utf8_vietnamese_ci;
 
 --
 -- Dumping data for table `brand`
 --
 
-INSERT INTO `brand` (`brandId`, `brand`) VALUES
-(1, 'Dell'),
-(2, 'Apple'),
-(3, 'Samsung'),
-(4, 'Microsoft'),
-(5, 'Xiaomi'),
-(6, 'Vivo'),
-(7, 'Oppo'),
-(8, 'Huawei'),
-(9, 'Realme'),
-(10, 'Lenovo'),
-(11, 'Asus'),
-(12, 'MSI'),
-(13, 'HP'),
-(14, 'Acer'),
-(15, 'Razer');
 
 -- --------------------------------------------------------
 
@@ -133,21 +117,18 @@ INSERT INTO `brand` (`brandId`, `brand`) VALUES
 
 DROP TABLE IF EXISTS `cart`;
 CREATE TABLE IF NOT EXISTS `cart` (
-  `userId` int(11) DEFAULT NULL,
+  `id_cart` int(11) NOT NULL AUTO_INCREMENT,
+  `id_account` int(11) DEFAULT NULL,
   `productId` int(11) DEFAULT NULL,
   `quantity` int(11) DEFAULT NULL,
-  `cartId` int(11) NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`cartId`),
-  KEY `cart_ibfk_1` (`userId`),
-  KEY `cart_ibfk_2` (`productId`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_vietnamese_ci;
+  PRIMARY KEY (`id_cart`)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8 COLLATE=utf8_vietnamese_ci;
 
 --
 -- Dumping data for table `cart`
 --
 
-INSERT INTO `cart` (`userId`, `productId`, `quantity`, `cartId`) VALUES
-(1, 1, 2, 1);
+
 
 -- --------------------------------------------------------
 
@@ -155,25 +136,20 @@ INSERT INTO `cart` (`userId`, `productId`, `quantity`, `cartId`) VALUES
 -- Table structure for table `comment`
 --
 
-DROP TABLE IF EXISTS `comment`;
-CREATE TABLE IF NOT EXISTS `comment` (
-  `commentId` int(11) NOT NULL AUTO_INCREMENT,
+-- chưa biết kết nối ở đâu
+DROP TABLE IF EXISTS `rate`;
+CREATE TABLE IF NOT EXISTS `rate` (
+  `id_rate` int(11) NOT NULL AUTO_INCREMENT,
   `productId` int(11) DEFAULT NULL,
-  `userId` int(11) DEFAULT NULL,
-  `content` varchar(1000) COLLATE utf8_vietnamese_ci DEFAULT NULL,
-  `time` timestamp NOT NULL DEFAULT current_timestamp(),
   `rate` double DEFAULT NULL,
-  PRIMARY KEY (`commentId`),
-  KEY `userId` (`userId`),
-  KEY `productId` (`productId`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_vietnamese_ci;
+  PRIMARY KEY (`id_rate`)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8 COLLATE=utf8_vietnamese_ci;
 
 --
 -- Dumping data for table `comment`
 --
 
-INSERT INTO `comment` (`commentId`, `productId`, `userId`, `content`, `time`, `rate`) VALUES
-(1, 1, 1, 'HSDGHAGSDAKGDHASGDAKSD', '2021-11-16 12:06:49', 4.5);
+
 
 -- --------------------------------------------------------
 
@@ -183,30 +159,22 @@ INSERT INTO `comment` (`commentId`, `productId`, `userId`, `content`, `time`, `r
 
 DROP TABLE IF EXISTS `product`;
 CREATE TABLE IF NOT EXISTS `product` (
-  `productId` int(11) NOT NULL AUTO_INCREMENT,
+  `id_product` int(11) NOT NULL AUTO_INCREMENT,
+  `id_category` int(11) DEFAULT NULL,
   `name` varchar(1000) COLLATE utf8_vietnamese_ci DEFAULT NULL,
   `description` varchar(1000) COLLATE utf8_vietnamese_ci DEFAULT NULL,
   `newPrice` int(11) DEFAULT NULL,
-  `price` int(11) DEFAULT NULL,
+  `oldprice` int(11) DEFAULT NULL,
   `rate` double DEFAULT NULL,
-  `brandId` int(11) DEFAULT NULL,
   `sale` tinyint(1) DEFAULT NULL,
-  `image` varchar(100) COLLATE utf8_vietnamese_ci NOT NULL DEFAULT 'product.webp',
-  `registerTime` date NOT NULL DEFAULT current_timestamp(),
-  `typeId` int(11) DEFAULT NULL,
-  `quantity` int(11) DEFAULT NULL,
   `sellQuantity` int(11) DEFAULT NULL,
-  PRIMARY KEY (`productId`),
-  KEY `typeId` (`typeId`),
-  KEY `brandId` (`brandId`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_vietnamese_ci;
+  PRIMARY KEY (`id_product`)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8 COLLATE=utf8_vietnamese_ci;
 
 --
 -- Dumping data for table `product`
 --
 
-INSERT INTO `product` (`productId`, `name`, `description`, `newPrice`, `price`, `rate`, `brandId`, `sale`, `image`, `registerTime`, `typeId`, `quantity`, `sellQuantity`) VALUES
-(1, 'Tai nghe True Wireless Chống ồn Soundpeats T2', 'Nhận gói 6 tháng Apple Music miễn phí và 1 km khác', 3500000, 3300000, 4, 1, 1, 'sound.webp', '2021-11-16', 2, 1000, 300);
 
 -- --------------------------------------------------------
 
@@ -216,72 +184,14 @@ INSERT INTO `product` (`productId`, `name`, `description`, `newPrice`, `price`, 
 
 DROP TABLE IF EXISTS `productimg`;
 CREATE TABLE IF NOT EXISTS `productimg` (
-  `productId` int(11) DEFAULT NULL,
-  `img1` varchar(100) COLLATE utf8_vietnamese_ci DEFAULT 'avatar.webp',
-  `img2` varchar(100) COLLATE utf8_vietnamese_ci DEFAULT 'avatar.webp',
-  `img3` varchar(100) COLLATE utf8_vietnamese_ci DEFAULT 'avatar.webp',
-  `img4` varchar(100) COLLATE utf8_vietnamese_ci DEFAULT 'avatar.webp',
-  UNIQUE KEY `productId` (`productId`)
+  `id_product` int(11) DEFAULT NULL,
+  `Imagepath` varchar(100) COLLATE utf8_vietnamese_ci DEFAULT 'avatar.webp',
+  PRIMARY KEY (`id_product`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_vietnamese_ci;
 
---
--- Dumping data for table `productimg`
---
 
-INSERT INTO `productimg` (`productId`, `img1`, `img2`, `img3`, `img4`) VALUES
-(1, 'avatar.webp', 'avatar1.webp', 'avatar2.webp', 'avatar3.webp');
 
--- --------------------------------------------------------
 
---
--- Table structure for table `token`
---
-
-DROP TABLE IF EXISTS `token`;
-CREATE TABLE IF NOT EXISTS `token` (
-  `email` varchar(100) COLLATE utf8_vietnamese_ci DEFAULT NULL,
-  `token` varchar(10000) COLLATE utf8_vietnamese_ci NOT NULL,
-  `duration` int(11) NOT NULL DEFAULT 60,
-  KEY `email` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_vietnamese_ci;
-
---
--- Dumping data for table `token`
---
-
-INSERT INTO `token` (`email`, `token`, `duration`) VALUES
-('nguyenvanlinh1@gmail.com', 'HSDGFHSHGDAHDHASHKASDHSGHAS', 60);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `type`
---
-
-DROP TABLE IF EXISTS `type`;
-CREATE TABLE IF NOT EXISTS `type` (
-  `typeId` int(11) NOT NULL AUTO_INCREMENT,
-  `typeName` varchar(100) COLLATE utf8_vietnamese_ci DEFAULT NULL,
-  PRIMARY KEY (`typeId`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8 COLLATE=utf8_vietnamese_ci;
-
---
--- Dumping data for table `type`
---
-
-INSERT INTO `type` (`typeId`, `typeName`) VALUES
-(1, 'mobile'),
-(2, 'laptop'),
-(3, 'tablet'),
-(4, 'sound'),
-(5, 'watch'),
-(6, 'monitor'),
-(7, 'pc'),
-(8, 'accessory'),
-(9, 'card'),
-(10, 'camera'),
-(11, 'keyboard'),
-(12, 'mouse');
 
 -- --------------------------------------------------------
 
@@ -289,80 +199,30 @@ INSERT INTO `type` (`typeId`, `typeName`) VALUES
 -- Table structure for table `user`
 --
 
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE IF NOT EXISTS `user` (
-  `userId` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(100) COLLATE utf8_vietnamese_ci DEFAULT NULL,
-  `password` varchar(100) COLLATE utf8_vietnamese_ci DEFAULT NULL,
-  `name` varchar(100) COLLATE utf8_vietnamese_ci DEFAULT NULL,
-  `email` varchar(100) COLLATE utf8_vietnamese_ci DEFAULT NULL,
-  `phone` varchar(20) COLLATE utf8_vietnamese_ci DEFAULT NULL,
-  `address` varchar(100) COLLATE utf8_vietnamese_ci DEFAULT NULL,
-  `avatar` varchar(100) COLLATE utf8_vietnamese_ci DEFAULT 'avatar.webp',
-  PRIMARY KEY (`userId`),
-  UNIQUE KEY `email` (`email`),
-  UNIQUE KEY `phone` (`phone`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_vietnamese_ci;
+DROP TABLE IF EXISTS `warehouse`;
+CREATE TABLE IF NOT EXISTS `warehouse` (
+  `id_product` int(11) NOT NULL,
+  `qty` int(11) NOT NULL,
+  `importTime` date NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id_product`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_vietnamese_ci;
 
---
--- Dumping data for table `user`
---
+DROP TABLE IF EXISTS `productDetail`;
+CREATE TABLE IF NOT EXISTS `productDetail` (
+  `id_product` int(11) NOT NULL,
+  `screen` varchar(100) COLLATE utf8_vietnamese_ci DEFAULT NULL,
+  `feature` varchar(100) COLLATE utf8_vietnamese_ci DEFAULT NULL,
+  `cpu` varchar(100) COLLATE utf8_vietnamese_ci DEFAULT NULL,
+  `ram` varchar(100) COLLATE utf8_vietnamese_ci DEFAULT NULL,
+  `weight` varchar(100) COLLATE utf8_vietnamese_ci DEFAULT NULL,
+  `camera` varchar(100) COLLATE utf8_vietnamese_ci DEFAULT NULL,
+  `storage` varchar(100) COLLATE utf8_vietnamese_ci DEFAULT NULL,
+  `bluetooth` varchar(100) COLLATE utf8_vietnamese_ci DEFAULT NULL,
+  `pin` varchar(100) COLLATE utf8_vietnamese_ci DEFAULT NULL,
+  `port` varchar(100) COLLATE utf8_vietnamese_ci DEFAULT NULL,
+  `resolution` varchar(100) COLLATE utf8_vietnamese_ci DEFAULT NULL,
+  PRIMARY KEY (`id_product`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_vietnamese_ci;
 
-INSERT INTO `user` (`userId`, `username`, `password`, `name`, `email`, `phone`, `address`, `avatar`) VALUES
-(1, 'vanlinh\r\n', 'vanlinh', 'Nguyễn Văn Linh', 'nguyenvanlinh1@gmail.com', '46646545645644', '134 Lê Đức Thọ, P. Tân Phong, Quận 7, TP. Hồ Chí Minh', 'avatar.webp');
 
---
--- Constraints for dumped tables
---
 
---
--- Constraints for table `bill`
---
--- ALTER TABLE `bill`
---   ADD CONSTRAINT `bill_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `user` (`userId`),
---   ADD CONSTRAINT `bill_ibfk_2` FOREIGN KEY (`userId`) REFERENCES `user` (`userId`);
-
---
--- Constraints for table `bill_detail`
---
-ALTER TABLE `bill_detail`
-  ADD CONSTRAINT `bill_detail_ibfk_1` FOREIGN KEY (`billId`) REFERENCES `bill` (`billId`),
-  ADD CONSTRAINT `bill_detail_ibfk_2` FOREIGN KEY (`billId`) REFERENCES `bill` (`billId`);
-
---
--- Constraints for table `cart`
---
-ALTER TABLE `cart`
-  ADD CONSTRAINT `cart_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `user` (`userId`) ON DELETE CASCADE,
-  ADD CONSTRAINT `cart_ibfk_2` FOREIGN KEY (`productId`) REFERENCES `product` (`productId`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `comment`
---
-ALTER TABLE `comment`
-  ADD CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `user` (`userId`) ON DELETE CASCADE,
-  ADD CONSTRAINT `comment_ibfk_2` FOREIGN KEY (`productId`) REFERENCES `product` (`productId`) ON DELETE CASCADE;
-
---
--- Constraints for table `product`
---
-ALTER TABLE `product`
-  ADD CONSTRAINT `product_ibfk_1` FOREIGN KEY (`typeId`) REFERENCES `type` (`typeId`),
-  ADD CONSTRAINT `product_ibfk_2` FOREIGN KEY (`brandId`) REFERENCES `brand` (`brandId`);
-
---
--- Constraints for table `productimg`
---
-ALTER TABLE `productimg`
-  ADD CONSTRAINT `productimg_ibfk_1` FOREIGN KEY (`productId`) REFERENCES `product` (`productId`) ON DELETE CASCADE;
-
---
--- Constraints for table `token`
---
-ALTER TABLE `token`
-  ADD CONSTRAINT `token_ibfk_1` FOREIGN KEY (`email`) REFERENCES `user` (`email`) ON DELETE CASCADE;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
